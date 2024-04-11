c4623f686c7f2df22c24d20502aa2df22cc9e707
kbopub <147172816+kbopub@users.noreply.github.com>
2024-04-11 19:24:34 +0200

    Add files via upload

---
+++ src/main/java/org/example/FenetreAffichage.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class FenetreAffichage extends JFrame {
+
+    PanelAffichage panAffich = new PanelAffichage();
+
+    public static void main(String[] args) {
+        new FenetreAffichage();
+    }
+    public FenetreAffichage(){
+
+        setVisible(true);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        setSize(500,500);
+        setLocationRelativeTo(null);
+        add(panAffich);
+        pack();
+    }
+}
