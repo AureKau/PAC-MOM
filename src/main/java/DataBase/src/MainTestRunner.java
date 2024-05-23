b2c09be5ee98d3c552508f090cb7b53ff8499261
ahmya <ahmya.i02@gmail.com>
2024-05-23 20:37:57 +0200

    23-05-24

--- src/main/java/DataBase/src/MainTestRunner.java
+++ src/main/java/DataBase/src/MainTestRunner.java
@@ -2,6 +2,7 @@ package DataBase.src;
 
 public class MainTestRunner {
 
+    public static GestionDB g1 = new GestionDB();
     public static void main(String[] args) {
         testCreateDatabase();
         testExecuteSqlInsertOrUpdate();
@@ -14,7 +15,7 @@ public class MainTestRunner {
 
     public static void testCreateDatabase() {
         try {
-            GestionDB gestionDB = new GestionDB();
+            GestionDB gestionDB = g1;
             gestionDB.createDataBase();
             System.out.println("Test testCreateDatabase réussi.");
         } catch (Exception e) {
@@ -24,7 +25,7 @@ public class MainTestRunner {
 
     public static void testExecuteSqlInsertOrUpdate() {
         try {
-            GestionDB gestionDB = new GestionDB();
+            GestionDB gestionDB = g1;
             gestionDB.executeSqlInsertOrUpdate("INSERT INTO Note (titre, texte) VALUES ('valeur', 'valeur')");
             System.out.println("Test testExecuteSqlInsertOrUpdate réussi.");
         } catch (Exception e) {
@@ -34,7 +35,7 @@ public class MainTestRunner {
 
     public static void testExecuteSqlSelect() {
         try {
-            GestionDB gestionDB = new GestionDB();
+            GestionDB gestionDB = g1;
             gestionDB.executeSqlSelect("SELECT * FROM note");
             System.out.println("Test testExecuteSqlSelect réussi.");
         } catch (Exception e) {
@@ -44,7 +45,7 @@ public class MainTestRunner {
 
     public static void testSaveNoteWithAll() {
         try {
-            GestionDB gestionDB = new GestionDB();
+            GestionDB gestionDB = g1;
             gestionDB.saveNoteWithAll("Auteur3", "Livre3", "Note3", "Contenu de la note3");
             System.out.println("Test testSaveNoteWithAll réussi.");
         } catch (Exception e) {
@@ -54,7 +55,7 @@ public class MainTestRunner {
 
     public static void testSaveNoteWithBook() {
         try {
-            GestionDB gestionDB = new GestionDB();
+            GestionDB gestionDB = g1;
             gestionDB.saveNoteWithBook("Livre4", "Note4", "Contenu de la note4");
             System.out.println("Test testSaveNoteWithBook réussi.");
         } catch (Exception e) {
@@ -64,7 +65,7 @@ public class MainTestRunner {
 
     public static void testSaveNoteOnly() {
         try {
-            GestionDB gestionDB = new GestionDB();
+            GestionDB gestionDB = g1;
             gestionDB.saveNoteOnly("Note5", "Contenu de la note5");
             System.out.println("Test testSaveNoteOnly réussi.");
         } catch (Exception e) {
@@ -74,7 +75,7 @@ public class MainTestRunner {
 
     public static void testRecupNameTableAndColumn() {
         try {
-            GestionDB gestionDB = new GestionDB();
+            GestionDB gestionDB = g1;
             gestionDB.toString();
             System.out.println("Test testRecupNameTableAndColumn réussi.");
         } catch (Exception e) {
